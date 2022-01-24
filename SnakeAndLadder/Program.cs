8a3e3ac0b2a5cee134de882b3a5ac79664ecec83
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-01-24 20:37:01 +0530

    [Raj] Added:- The die roll to get numbers from one to six

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -10,10 +10,15 @@ namespace SnakeAndLadder
     {
         //Initializing single player position(UC1)
         public static int startPosition = 0;
+        public static Random dieBoxRoll = new Random();
         public static void Main(string[] args)
         {
             //Displaying message
             Console.WriteLine("Welcome to the snake and ladder program");
+
+            //The Player rolls the die to get a number between 1 to 6(UC2)
+            int rollDie = dieBoxRoll.Next(1, 7);
+            Console.WriteLine("The Die Rolled at : "+rollDie);
             Console.ReadLine();
         }
     }
