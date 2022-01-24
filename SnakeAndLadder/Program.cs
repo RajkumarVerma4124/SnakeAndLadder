ded1874e79dff9926205de9294d2d2946ed7403f
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-01-24 20:21:10 +0530

    [Raj] Added:- The welcome message

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Displaying message
+            Console.WriteLine("Welcome to the snake and ladder program");
+            Console.ReadLine();
+        }
+    }
+}
